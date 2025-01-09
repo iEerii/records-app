297a79d7ac79603f78209f7f3501b8a7008bfa46
Ericka Ponce <erickaponce92@gmail.com>
2025-01-09 18:13:43 -0500

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import Principal from './components/Principal'
+import { Routes, Route } from 'react-router';
+import styled from 'styled-components';
+import LoginWarp from './components/LoginWrap';
+import SignUp from './components/SignUp';
 import './App.css'
 
 function App() {
 
   return (
-		<>
-			<Principal />
-		</>
+		<Main>
+			<Routes>
+					<Route path="/login" element={ <LoginWarp /> }></Route>
+					<Route path="/signup" element={ <SignUp /> }></Route>
+					{/* <Route path="*" element={ <App/> }/> */}
+			</Routes>
+		</Main>
 	)
 }
+
+const Main = styled.main`
+    align-items: center;
+    background-color: #102c97;
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+`;
 
 export default App
