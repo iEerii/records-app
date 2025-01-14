328d6188146597ca7d71d66da0837ecc6b017237
Ericka Ponce <erickaponce92@gmail.com>
2025-01-14 13:09:57 -0500

    change main name component

--- src/components/LoginWrap.tsx
+++ src/components/LoginWrap.tsx
@@ -1,28 +1,13 @@
 import Form from "./Form";
-import styled from "styled-components";
+import { MainWrap, Title } from '../elements/FormElements'
 
 const LoginWrap = () => {
     return (
-        <Login>
+        <MainWrap>
             <Title>Login</Title>
             <Form />
-        </Login>
+        </MainWrap>
     );
 }
 
-const Login = styled.div`
-    align-items: center;
-    background-color: #FFFFFF;
-    border-radius: 2rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 40rem;
-`
-
-const Title = styled.h1`
-    color: #4b4b4b;
-    font-size: 4rem;
-    margin: 6rem 0 5rem;        
-`
 export default LoginWrap;
